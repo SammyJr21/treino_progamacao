463a85f9564277c68ff5975a4e84af32c000e38b
SammyJr21 <sammyjuniorgt@gmail.com>
2021-10-14 15:45:52 -0300

    testando

---
+++ src/br/com/alura/TesteAlunos.java
@@ -0,0 +1,26 @@
+package br.com.alura;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class TesteAlunos {
+	public static void main(String[] args) {
+		Collection<String> alunos = new HashSet<>();
+		alunos.add("Rodrigo Turini");
+		alunos.add("Alberto Souza");
+		alunos.add("Nico Steppat");
+		alunos.add("Sergio Lopes");
+		alunos.add("Renan Saggio");
+		alunos.add("Mauricio Aniche");
+
+		for (String aluno : alunos) {
+			System.out.println(aluno);
+		}
+
+		alunos.forEach(aluno -> {
+			System.out.println(aluno);
+		});
+
+		System.out.println(alunos);
+	}
+}
